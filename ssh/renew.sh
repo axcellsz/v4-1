98ae5e14f260cb95e8f3d5fc20aa643821f5d1f2
artanodrop <114789247+artanodrop@users.noreply.github.com>
2022-10-26 17:31:12 +0700

    Update renew.sh

--- ssh/renew.sh
+++ ssh/renew.sh
@@ -43,3 +43,6 @@ echo -e "   Username Doesnt Exist      "
 echo -e ""
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 fi
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
+
