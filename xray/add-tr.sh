ec05552bdb6139f2dff1d017426d297afcfbc0b3
artanodrop <114789247+artanodrop@users.noreply.github.com>
2023-05-09 06:09:39 +0700

    Update add-tr.sh

--- xray/add-tr.sh
+++ xray/add-tr.sh
@@ -46,11 +46,11 @@ trojanlink1="trojan://${uuid}@${domain}:${tr}?mode=gun&security=tls&type=grpc&se
 trojanlink="trojan://${uuid}@isi_bug_disini:${tr}?path=%2Ftrojan-ws&security=tls&host=${domain}&type=ws&sni=${domain}#${user}"
 clear
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\E[40;1;37m           Trial TROJAN           \E[0m"
+echo -e "\E[40;1;37m        TROJAN ACCOUNT          \E[0m"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e "Remarks      : ${user}"
 echo -e "Host/IP      : ${domain}"
-echo -e "port         : ${tr}"
+echo -e "port         : 443/80"
 echo -e "Key          : ${uuid}"
 echo -e "Path         : /trojan-ws"
 echo -e "ServiceName  : trojan-grpc"
