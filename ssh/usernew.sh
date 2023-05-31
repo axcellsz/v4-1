bf59d7c6d24b5434253c7bd6064d929f923f0458
artanodrop <114789247+artanodrop@users.noreply.github.com>
2023-05-31 20:23:38 +0700

    Update usernew.sh

--- ssh/usernew.sh
+++ ssh/usernew.sh
@@ -83,6 +83,7 @@ echo -e "SSH-WS      : 80" | tee -a /etc/log-create-user.log
 echo -e "SSH-SSL-WS  : 443" | tee -a /etc/log-create-user.log
 echo -e "SSL/TLS     : 447 , 777" | tee -a /etc/log-create-user.log
 echo -e "UDPGW       : 7100-7300" | tee -a /etc/log-create-user.log
+echo -e "SSH-UDP     : 56-65545" | tee -a /etc/log-create-user.log
 #echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
 #echo -e "OpenVPN Config : http://$IP:81/" | tee -a /etc/log-create-user.log
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m" | tee -a /etc/log-create-user.log
